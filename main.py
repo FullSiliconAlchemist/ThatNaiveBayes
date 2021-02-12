e9cbec792959abfa6b36a2b3868dda9f11da2016
Charles Rabbat <chuckthedunker@gmail.com>
2021-02-11 21:24:52 -0500

    finished task 1 for assignment 1

---
+++ main.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import math
+import re
+
+data = pd.read_csv("data.txt", sep="\t")
+
+# Split data into two groups, one for training one for testing
+training = math.floor(data.shape[0] * 0.8)
+
+training_data = data.iloc[:, :training]
+testing_data = data.iloc[:, training:]
+
+training_data_arr = []
+
+# TASK 1
+num_neg = 0
+num_pos = 0
+
+# Remove unnecessary columns and format into multi-dimensional array
+for index, row in training_data.iterrows():
+    # Get class
+    data_class = [row[0].split(" ", 1)[1].split(" ", 2)[0]]
+    # Count number of classes
+    if data_class[0] == 'neg':
+        num_neg += 1
+    elif data_class[0] == 'pos':
+        num_pos += 1
+    # Get data and remove all non-words, then split by spaces
+    fields = row[0].split(" ", 1)[1].split(" ", 2)[2]
+    data_fields = re.sub('[\W_]+[ ]', '', fields).split(" ")
+    data_class.append(data_fields)
+    training_data_arr.append(data_class)
+
+print('Data cleaning complete')
+
+# Distribution of positive and negative reviews
+objects = ('Positive', 'Negative')
+y_pos = np.arange(len(objects))
+performance = [num_neg, num_pos]
+
+# Styling
+plt.bar(y_pos, performance, align='center', alpha=0.5)
+plt.xticks(y_pos, objects)
+plt.ylabel('Frequency')
+plt.title('Frequency of Negative and Positive Reviews')
+
+# Show plot results
+plt.show()
+
+# Naive bayes classification
+
+
+# How it works:
+# P(Negative | W1 AND W2 AND W3 AND ... WN) = P(Negative) * P(W1 | Negative) * P(W2 | Negative)
+
+print('\nDone')
+
