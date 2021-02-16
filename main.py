1fdec9f70ddcc7a4ad838f9f8e7c03e754e4ef5b
Charles Rabbat <chuckthedunker@gmail.com>
2021-02-16 14:18:19 -0500

    used scikit learn to train and test naive bayes algorithm

--- main.py
+++ main.py
@@ -6,15 +6,28 @@ import math
 import re
 
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
 from collections import Counter
 
 
 # Task 1 method for plotting frequencies
-def plot_frequencies(data_pos, data_neg):
+def plot_frequencies(data):
+    num_neg = 0
+    num_pos = 0
+
+    for row in data:
+        data_class = row[0].split(" ", 1)[1].split(" ", 2)[0]
+        # Count number of classes
+        if data_class == 'neg':
+            num_neg += 1
+        elif data_class == 'pos':
+            num_pos += 1
+
     # Distribution of positive and negative reviews
     objects = ('Positive', 'Negative')
     y_pos = np.arange(len(objects))
-    performance = [data_pos, data_neg]
+    performance = [num_pos, num_neg]
 
     # Styling
     plt.bar(y_pos, performance, align='center', alpha=0.5)
@@ -25,48 +38,15 @@ def plot_frequencies(data_pos, data_neg):
     # Show plot results
     # plt.show()
 
-
-data = pd.read_csv("all_sentiment_shuffled.txt", sep="\t")
-
+# Import data using pandas
+data = pd.read_csv("all_sentiment_shuffled.txt", sep="\t", header=None)
+# Convert to numpy object for fast iteration
 np_data = data.to_numpy()
 
-num_neg = 0
-num_pos = 0
+# Plot class frequencies method
+plot_frequencies(np_data)
+print('Plotting complete')
 
-# Best not to iterate over dataframes, should convert large datasets to vectors
-for x in range(np_data.shape[0]):
-    data_class = np_data[x][0].split(" ", 1)[1].split(" ", 2)[0]
-    # Count number of classes
-    if data_class == 'neg':
-        num_neg += 1
-    elif data_class == 'pos':
-        num_pos += 1
-
-# Split data into two groups, one for training one for testing
-training = math.floor(np_data.shape[0] * 0.8)
-
-training_data_v1 = []
-training_data_v2 = [[], []]
-
-# Remove unnecessary columns and format into multi-dimensional array
-for x in range(training):
-    # Get class
-    data_class = [np_data[x][0].split(" ", 1)[1].split(" ", 2)[0]]
-    # Get data and remove all non-words, then split by spaces
-    fields = np_data[x][0].split(" ", 1)[1].split(" ", 2)[2]
-    data_fields = re.sub('[\W_]+[ ]', '', fields).split(" ")
-    data_class.append(data_fields)
-    training_data_v1.append(data_class)
-    # V2 training
-    if data_class[0] == 'pos':
-        training_data_v2[0].append(data_fields)
-    if data_class[0] == 'neg':
-        training_data_v2[1].append(data_fields)
-
-
-print('Data cleaning complete')
-
-plot_frequencies(num_pos, num_neg)
 
 # Multinomial Naive bayes classification
 # P(Negative | W1 AND W2 AND W3 AND ... WN) = P(Negative) * P(W1 | Negative) * P(W2 | Negative)
@@ -76,30 +56,38 @@ plot_frequencies(num_pos, num_neg)
 #   ('W2', 12),
 #   etc...
 # ]
-neg_freq = Counter()
-pos_freq = Counter()
-tot_words = Counter()
 
-for data in training_data_v1:
-    # Get all words in vocab
-    for w in data[1]:
-        tot_words[w] += 1
-    # Get all occurrences of negative words
-    if data[0] == 'neg':
-        for w in data[1]:
-            neg_freq[w] += 1
-    # Get all occurrences of positive words
-    if data[0] == 'pos':
-        for w in data[1]:
-            pos_freq[w] += 1
+refactored_data = []
 
-tot_vocab = len(tot_words.values())
+# Remove unnecessary columns and format into multi-dimensional array
+for row in np_data:
+    # Get class
+    data_class = [row[0].split(" ", 1)[1].split(" ", 2)[0]]
+    # Get data and remove all non-words, then split by spaces
+    fields = row[0].split(" ", 1)[1].split(" ", 2)[2]
+    data_fields = re.sub('[\W_]+[ ]', '', fields)
+    data_class.append(fields)
+    refactored_data.append(data_class)
+
+# Put refactored data into a new dataframe object and define the column names for scikit
+df = pd.DataFrame(refactored_data)
+df.columns = ['CATEGORY', 'CONTENT']
+
+# Filter stop words in the english language
+vectorizer = CountVectorizer(stop_words='english')
+
+# Create dictionary object with vocabulary and word frequency
+all_features = vectorizer.fit_transform(df.CONTENT)
+
+# Split training and test data
+X_train, X_test, y_train, y_test = train_test_split(all_features, df.CATEGORY, test_size=0.8)
+print('Training complete')
 
 nb = MultinomialNB()
-np_arr = np.array(training_data_v2)
-nb.fit(np_arr, ['pos', 'neg'])
-
-# print(training_data_v1)
+nb.fit(X_train, y_train)
+correct = (y_test == nb.predict(X_test)).sum()
+print(f'{correct/len(y_test)*100}% of reviews classified correctly')
+# print(nb.predict(X_test))
 
 # Base Decision Tree
 
