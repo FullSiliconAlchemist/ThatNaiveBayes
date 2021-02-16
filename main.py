486814174bf40c8e441bd0924dea41b4b24a7162
Charles Rabbat <chuckthedunker@gmail.com>
2021-02-15 21:27:47 -0500

    trying to make sense of scikit algorithms

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 import matplotlib.pyplot as plt; plt.rcdefaults()
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import math
 import re
+
+from sklearn.naive_bayes import MultinomialNB
+from collections import Counter
+
+
+# Task 1 method for plotting frequencies
+def plot_frequencies(data_pos, data_neg):
+    # Distribution of positive and negative reviews
+    objects = ('Positive', 'Negative')
+    y_pos = np.arange(len(objects))
+    performance = [data_pos, data_neg]
+
+    # Styling
+    plt.bar(y_pos, performance, align='center', alpha=0.5)
+    plt.xticks(y_pos, objects)
+    plt.ylabel('Frequency')
+    plt.title('Frequency of Negative and Positive Reviews')
+
+    # Show plot results
+    # plt.show()
+
 
 data = pd.read_csv("all_sentiment_shuffled.txt", sep="\t")
 
 np_data = data.to_numpy()
 
-# TASK 1
 num_neg = 0
 num_pos = 0
 
@@ -25,11 +45,8 @@ for x in range(np_data.shape[0]):
 # Split data into two groups, one for training one for testing
 training = math.floor(np_data.shape[0] * 0.8)
 
-# Slicing of different data groups
-training_data = data.iloc[:training]
-testing_data = data.iloc[training:]
-
-training_data_arr = []
+training_data_v1 = []
+training_data_v2 = [[], []]
 
 # Remove unnecessary columns and format into multi-dimensional array
 for x in range(training):
@@ -39,29 +56,53 @@ for x in range(training):
     fields = np_data[x][0].split(" ", 1)[1].split(" ", 2)[2]
     data_fields = re.sub('[\W_]+[ ]', '', fields).split(" ")
     data_class.append(data_fields)
-    training_data_arr.append(data_class)
+    training_data_v1.append(data_class)
+    # V2 training
+    if data_class[0] == 'pos':
+        training_data_v2[0].append(data_fields)
+    if data_class[0] == 'neg':
+        training_data_v2[1].append(data_fields)
+
 
 print('Data cleaning complete')
 
-# Distribution of positive and negative reviews
-objects = ('Positive', 'Negative')
-y_pos = np.arange(len(objects))
-performance = [num_pos, num_neg]
+plot_frequencies(num_pos, num_neg)
 
-# Styling
-plt.bar(y_pos, performance, align='center', alpha=0.5)
-plt.xticks(y_pos, objects)
-plt.ylabel('Frequency')
-plt.title('Frequency of Negative and Positive Reviews')
-
-# Show plot results
-plt.show()
-
-# Naive bayes classification
-
-
-# How it works:
+# Multinomial Naive bayes classification
 # P(Negative | W1 AND W2 AND W3 AND ... WN) = P(Negative) * P(W1 | Negative) * P(W2 | Negative)
+# P(W1 | Negative) = (# of times W1 appears in training data of negative class) / (# of words)
+# [
+#   ('W1', 10),
+#   ('W2', 12),
+#   etc...
+# ]
+neg_freq = Counter()
+pos_freq = Counter()
+tot_words = Counter()
+
+for data in training_data_v1:
+    # Get all words in vocab
+    for w in data[1]:
+        tot_words[w] += 1
+    # Get all occurrences of negative words
+    if data[0] == 'neg':
+        for w in data[1]:
+            neg_freq[w] += 1
+    # Get all occurrences of positive words
+    if data[0] == 'pos':
+        for w in data[1]:
+            pos_freq[w] += 1
+
+tot_vocab = len(tot_words.values())
+
+nb = MultinomialNB()
+np_arr = np.array(training_data_v2)
+nb.fit(np_arr, ['pos', 'neg'])
+
+# print(training_data_v1)
+
+# Base Decision Tree
+
+# Optimized Decision Tree (Entropy measurements)
 
 print('\nDone')
-
